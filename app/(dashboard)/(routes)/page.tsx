19d68ae43fd574ac62fe6098a9ab2d01f6c4cc22
Marcin Siwonia <mahinek12@gmail.com>
2023-11-02 18:43:50 +0100

    Add popularServices

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,11 +1,12 @@
 
 import Hero from "./_components/Hero"
+import PopularServices from "./_components/PopularServices"
 
 export default function Home() {
   return (
     <div>
  <Hero />
-
+<PopularServices />
     </div>
 
   )
